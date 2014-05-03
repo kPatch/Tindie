bcf91b8c1806552edea402f3ca9701e0d1002ca2
kPatch <irvsteve@gmail.com>
2014-05-03 12:22:02 -0400

    Testing Git.

--- gen/com/activity/BuildConfig.java
+++ gen/com/activity/BuildConfig.java
@@ -1,13 +1,6 @@
 /** Automatically generated file. DO NOT MODIFY */
 package com.activity;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class BuildConfig.
- */
-public final class BuildConfig
-{
-	
-	/** The Constant DEBUG. */
-	public final static boolean DEBUG = true;
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
 }
