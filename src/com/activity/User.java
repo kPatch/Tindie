e17c94fcf22055788cfbfb43ba607f833b8c4414
kPatch <irvsteve@gmail.com>
2014-05-04 09:29:35 -0400

    User Settings

---
+++ src/com/activity/User.java
@@ -0,0 +1,173 @@
+package com.activity;
+
+import java.io.IOException;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.activity.custom.CustomActivity;
+
+/**
+ * The Class Login is an Activity class that shows the login screen to users.
+ * The current implementation simply start the MainActivity. You can write your
+ * own logic for actual login and for login using Facebook and Twitter.
+ */
+public class User extends CustomActivity {
+
+	private static final int CAMERA_REQUEST = 500;
+	private ImageView imageView;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.chatt.custom.CustomActivity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user);
+
+		// setTouchNClick(R.id.btnLogin);
+		// setTouchNClick(R.id.btnFb);
+		// setTouchNClick(R.id.btnTw);
+		// setTouchNClick(R.id.btnReg);
+		setTouchNClick(R.id.btnRegSave);
+
+		imageView = (ImageView) findViewById(R.id.userImage);
+		Button bt = (Button) findViewById(R.id.btCam);
+
+		bt.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+				startActivityForResult(intent, CAMERA_REQUEST);
+
+			}
+		});
+	}
+
+	//@Override
+	/*
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    // TODO Auto-generated method stub
+	    super.onActivityResult(requestCode, resultCode, data);
+
+	    if (resultCode == RESULT_OK) {
+
+	        switch (requestCode) {
+	        case CAMERA_IMAGE_REQUEST:
+
+	            Bitmap bitmap = null;
+	            try {
+	                GetImageThumbnail getImageThumbnail = new GetImageThumbnail();
+	                bitmap = getImageThumbnail.getThumbnail(fileUri, this);
+	            } catch (FileNotFoundException e1) {
+	                // TODO Auto-generated catch block
+	                e1.printStackTrace();
+	            } catch (IOException e1) {
+	                // TODO Auto-generated catch block
+	                e1.printStackTrace();
+	            }
+
+	            // Setting image image icon on the imageview
+
+	            ImageView imageView = (ImageView) this
+	                    .findViewById(R.id.capturedImageview);
+
+	            imageView.setImageBitmap(bitmap);
+
+	            break;
+
+	        default:
+	            Toast.makeText(this, "Something went wrong...",
+	                    Toast.LENGTH_SHORT).show();
+	            break;
+	        }
+
+	    }
+	}	
+	*/
+	
+	//@Override
+	
+	protected void onActivityResult(int requestCode, int resultCode,
+			final Intent imageReturnedIntent) {
+		super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+		switch (requestCode) {
+		case 1:
+			if (resultCode == RESULT_OK) {
+
+				try {
+					Uri selectedImage = imageReturnedIntent.getData();
+
+					Cursor cursor = MediaStore.Images.Thumbnails
+							.queryMiniThumbnails(getContentResolver(),
+									selectedImage,
+									MediaStore.Images.Thumbnails.MINI_KIND,
+									null);
+					if (cursor != null && cursor.getCount() > 0) {
+						cursor.moveToFirst();// **EDIT**
+						String uri = cursor
+								.getString(cursor
+										.getColumnIndex(MediaStore.Images.Thumbnails.DATA));
+					}
+
+					// bmpSelectedImage = getThumbnail(selectedImage);
+					//set_img_camera.setImageBitmap(bmpSelectedImage);
+					//ImageView img = (ImageView)findViewById(R.id.);
+					
+					final int THUMBSIZE = 64;
+
+					
+					
+					String[] filePathColumn = {MediaStore.Images.Media.DATA};
+					 int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+					 String filePath = cursor.getString(columnIndex);
+					 cursor.close();
+					 Bitmap bMap = BitmapFactory.decodeFile(filePath);
+					 
+					 
+					 Bitmap ThumbImage = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(filePath), 
+			                    THUMBSIZE, THUMBSIZE);
+					 
+					 ImageView theImageView = (ImageView) this
+			                    .findViewById(R.id.userImage);
+					//theImageView.setImageBitmap(bMap);
+					 theImageView.setImageBitmap(ThumbImage);
+
+				} catch (Exception e) {
+					Log.d("image error", e.toString());
+				}
+
+			}
+
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.chatt.custom.CustomActivity#onClick(android.view.View)
+	 */
+	@Override
+	public void onClick(View v) {
+		super.onClick(v);
+		if (v.getId() != R.id.btnReg) {
+			startActivity(new Intent(this, MainActivity.class));
+			finish();
+		}
+	}
+}
