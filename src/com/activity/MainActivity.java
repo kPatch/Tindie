9a375c2bfcb5ad90d9f8da3f98ed593a63d09378
kPatch <irvsteve@gmail.com>
2014-05-04 03:28:31 -0400

    Bluetooth Data.

--- src/com/activity/MainActivity.java
+++ src/com/activity/MainActivity.java
@@ -1,9 +1,19 @@
 package com.activity;
 
 // TEST: Irvin Cardenas
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
 import android.app.ActionBar;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -13,7 +23,10 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -29,8 +42,7 @@ import com.activity.ui.Workout;
  * views and Fragments used in the app.
  * 
  */
-public class MainActivity extends CustomActivity
-{
+public class MainActivity extends CustomActivity {
 
 	/** The view pager for swipe views. */
 	private ViewPager pager;
@@ -38,18 +50,50 @@ public class MainActivity extends CustomActivity
 	/** The current selected tab. */
 	private View currentTab;
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.newsfeeder.custom.CustomActivity#onCreate(android.os.Bundle)
 	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		System.out.println(" Before setupActionBar");
 		setupActionBar();
 		initTabs();
-		initPager();
+		initPager();//textView2_1
+		System.out.println(" Before myLabel");
+		/*
+		myLabel = (TextView)findViewById(R.id.MotionAngleValue);
+		ImageView button01 = (ImageView) findViewById(R.id.on);
+		System.out.println(" Before OnClickListener");
+		button01.setOnClickListener(new OnClickListener() {
+			int button01pos = 0;
+			public void onClick(View v) {
+				System.out.println("onClick");
+				if (button01pos == 0) {
+					// button01.setImageResource(R.drawable.image01);
+					try {
+						findBT();
+						openBT();
+					} catch (IOException ex) {
+					}
+					button01pos = 1;
+				} else if (button01pos == 1) {
+					// button01.setImageResource(R.drawable.image02);
+		            try 
+		            {
+		                closeBT();
+		            }
+		            catch (IOException ex) { }
+					button01pos = 0;
+				}
+				
+			}
+		});
+		*/
+		
 	}
 
 	/**
@@ -58,8 +102,7 @@ public class MainActivity extends CustomActivity
 	 * can override this method to change the behavior of ActionBar for
 	 * particular Activity
 	 */
-	protected void setupActionBar()
-	{
+	protected void setupActionBar() {
 		final ActionBar actionBar = getActionBar();
 		actionBar.setDisplayShowTitleEnabled(true);
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
@@ -69,12 +112,10 @@ public class MainActivity extends CustomActivity
 		actionBar.setHomeButtonEnabled(false);
 	}
 
-
 	/**
 	 * Initialize the tabs. You can write your code related to Tabs.
 	 */
-	private void initTabs()
-	{
+	private void initTabs() {
 		findViewById(R.id.tab1).setOnClickListener(this);
 		findViewById(R.id.tab2).setOnClickListener(this);
 		findViewById(R.id.tab3).setOnClickListener(this);
@@ -82,12 +123,13 @@ public class MainActivity extends CustomActivity
 		setCurrentTab(0);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.activity.custom.CustomActivity#onClick(android.view.View)
 	 */
 	@Override
-	public void onClick(View v)
-	{
+	public void onClick(View v) {
 		super.onClick(v);
 		if (v.getId() == R.id.tab1)
 			pager.setCurrentItem(0, true);
@@ -103,11 +145,11 @@ public class MainActivity extends CustomActivity
 	 * Sets the current selected tab. Called whenever a Tab is selected either
 	 * by clicking on Tab button or by swiping the ViewPager. You can write your
 	 * code related to tab selection actions.
-	 *
-	 * @param page the current page of ViewPager
+	 * 
+	 * @param page
+	 *            the current page of ViewPager
 	 */
-	private void setCurrentTab(int page)
-	{
+	private void setCurrentTab(int page) {
 		if (currentTab != null)
 			currentTab.setEnabled(true);
 		if (page == 0)
@@ -119,32 +161,28 @@ public class MainActivity extends CustomActivity
 		else
 			currentTab = findViewById(R.id.tab4);
 		currentTab.setEnabled(false);
-		getActionBar().setTitle(((Button)currentTab).getText().toString());
+		getActionBar().setTitle(((Button) currentTab).getText().toString());
 	}
 
 	/**
 	 * Initialize the ViewPager. You can customize this method for writing the
 	 * code related to view pager actions.
 	 */
-	private void initPager()
-	{
+	private void initPager() {
 		pager = (ViewPager) findViewById(R.id.pager);
 		pager.setOnPageChangeListener(new OnPageChangeListener() {
 
 			@Override
-			public void onPageSelected(int page)
-			{
+			public void onPageSelected(int page) {
 				setCurrentTab(page);
 			}
 
 			@Override
-			public void onPageScrolled(int arg0, float arg1, int arg2)
-			{
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
 			}
 
 			@Override
-			public void onPageScrollStateChanged(int arg0)
-			{
+			public void onPageScrollStateChanged(int arg0) {
 			}
 		});
 		pager.setAdapter(new DummyPageAdapter(getSupportFragmentManager()));
@@ -154,8 +192,7 @@ public class MainActivity extends CustomActivity
 	 * The Class DummyPageAdapter is a dummy pager adapter for ViewPager. You
 	 * can customize this adapter as per your needs.
 	 */
-	private class DummyPageAdapter extends FragmentPagerAdapter
-	{
+	private class DummyPageAdapter extends FragmentPagerAdapter {
 
 		/**
 		 * Instantiates a new dummy page adapter.
@@ -163,17 +200,17 @@ public class MainActivity extends CustomActivity
 		 * @param fm
 		 *            the FragmentManager
 		 */
-		public DummyPageAdapter(FragmentManager fm)
-		{
+		public DummyPageAdapter(FragmentManager fm) {
 			super(fm);
 		}
 
-		/* (non-Javadoc)
+		/*
+		 * (non-Javadoc)
+		 * 
 		 * @see android.support.v4.app.FragmentPagerAdapter#getItem(int)
 		 */
 		@Override
-		public Fragment getItem(int pos)
-		{
+		public Fragment getItem(int pos) {
 			if (pos == 0)
 				return new NewActivity();
 			if (pos == 1)
@@ -183,41 +220,45 @@ public class MainActivity extends CustomActivity
 			return new History();
 		}
 
-		/* (non-Javadoc)
+		/*
+		 * (non-Javadoc)
+		 * 
 		 * @see android.support.v4.view.PagerAdapter#getCount()
 		 */
 		@Override
-		public int getCount()
-		{
+		public int getCount() {
 			return 4;
 		}
 
 	}
 
-	/* (non-Javadoc)
-	 * @see com.newsfeeder.custom.CustomActivity#onCreateOptionsMenu(android.view.Menu)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.newsfeeder.custom.CustomActivity#onCreateOptionsMenu(android.view
+	 * .Menu)
 	 */
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	/* (non-Javadoc)
-	 * @see com.activity.custom.CustomActivity#onOptionsItemSelected(android.view.MenuItem)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.activity.custom.CustomActivity#onOptionsItemSelected(android.view
+	 * .MenuItem)
 	 */
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		if(item.getItemId()==R.id.menu_setting)
-		{
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.menu_setting) {
 			startActivity(new Intent(this, Setting.class));
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-
 }
--- src/com/activity/ui/NewActivity.java
+++ src/com/activity/ui/NewActivity.java
@@ -1,12 +1,25 @@
 package com.activity.ui;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.activity.R;
 
@@ -24,6 +37,22 @@ public class NewActivity extends Fragment
 			R.drawable.teddy_down, R.drawable.teddy_left};
 	private int tedctn = 1;
 	
+	/**** */
+	
+	EditText myTextbox;
+	BluetoothAdapter mBluetoothAdapter;
+	BluetoothSocket mmSocket;
+	BluetoothDevice mmDevice;
+	OutputStream mmOutputStream;
+	InputStream mmInputStream;
+	Thread workerThread;
+	byte[] readBuffer;
+	int readBufferPosition;
+	int counter;
+	volatile boolean stopWorker;
+	TextView myLabel;
+	
+	ImageView TeddyView;
 	/* (non-Javadoc)
 	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
 	 */
@@ -47,9 +76,10 @@ public class NewActivity extends Fragment
 	 */
 	private void setupView(View v)
 	{
-		final ImageView TeddyView = (ImageView) v.findViewById(R.id.imageView1);
+		TeddyView = (ImageView) v.findViewById(R.id.imageView1);
+		myLabel = (TextView)v.findViewById(R.id.textView2_1);
 		TeddyView.setOnClickListener(new OnClickListener() {
-
+	
 			@Override
 			public void onClick(View v)
 			{
@@ -62,21 +92,166 @@ public class NewActivity extends Fragment
 		
 		final View sw = v.findViewById(R.id.vSwitch);
 		sw.setOnClickListener(new OnClickListener() {
-
+			
+			
+			//TextView myLabel = (TextView)sw.findViewById(R.id.textView2_1);
+		    //myTextbox = (EditText)findViewById(R.id.entry);
 			@Override
 			public void onClick(View v)
 			{
 				if (sw.getTag() == null)
 				{
 					sw.setBackgroundResource(R.drawable.swith_up_right);
-					sw.setTag("gps");
+		            try 
+		            {
+		                findBT();
+		                openBT();
+		            }
+		            catch (IOException ex) { }
+
 				}
 				else
 				{
 					sw.setBackgroundResource(R.drawable.swith_up_left);
 					sw.setTag(null);
-				}
+					sw.setTag("gps");
+					try 
+		            {
+		            	closeBT();
+		            }
+		            catch (IOException ex) { }
+				}	
 			}
 		});
 	}
+	
+
+void findBT()
+{
+    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if(mBluetoothAdapter == null)
+    {
+        myLabel.setText("No bluetooth adapter available");
+    }
+
+    if(!mBluetoothAdapter.isEnabled())
+    {
+        Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBluetooth, 0);
+    }
+
+    Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+    if(pairedDevices.size() > 0)
+    {
+        for(BluetoothDevice device : pairedDevices)
+        {
+        	myLabel.setText("Search");
+            if(device.getName().equals("RNBT-38B5")) 
+            {
+            	myLabel.setText("YAY");
+                mmDevice = device;
+                break;
+            }
+        }
+    }
+    myLabel.setText("Bluetooth Device Found");
+}
+
+void openBT() throws IOException
+{
+	myLabel.setText("OpenBT");
+    UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+    mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);        
+    mmSocket.connect();
+    myLabel.setText("Connect");
+    mmOutputStream = mmSocket.getOutputStream();
+    mmInputStream = mmSocket.getInputStream();
+
+    beginListenForData();
+
+    myLabel.setText("Bluetooth Opened");
+    try 
+    {
+        sendData();
+    }
+    catch (IOException ex) { }
+}
+
+void beginListenForData()
+{
+    final Handler handler = new Handler(); 
+    final byte delimiter = 10; //This is the ASCII code for a newline character
+    myLabel.setText("Begin Listening");
+    stopWorker = false;
+    readBufferPosition = 0;
+    readBuffer = new byte[1024];
+    workerThread = new Thread(new Runnable()
+    {
+        public void run()
+        {                
+           while(!Thread.currentThread().isInterrupted() && !stopWorker)
+           {
+                try 
+                {
+                    int bytesAvailable = mmInputStream.available();                        
+                    if(bytesAvailable > 0)
+                    {
+                        byte[] packetBytes = new byte[bytesAvailable];
+                        mmInputStream.read(packetBytes);
+                        for(int i=0;i<bytesAvailable;i++)
+                        {
+                            byte b = packetBytes[i];
+                            if(b == delimiter)
+                            {
+     byte[] encodedBytes = new byte[readBufferPosition];
+     System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+     final String data = new String(encodedBytes, "US-ASCII");
+     //int roll = Integer.parseInt(data.split("\\s+")[2]);
+     readBufferPosition = 0;
+
+                                handler.post(new Runnable()
+                                {
+                                    public void run()
+                                    {
+                                        myLabel.setText(data);
+                                        //if(roll )
+                                        //TeddyView.setImageResource(tedarr[0]);
+                                    }
+                                });
+                            }
+                            else
+                            {
+                                readBuffer[readBufferPosition++] = b;
+                            }
+                        }
+                    }
+                } 
+                catch (IOException ex) 
+                {
+                    stopWorker = true;
+                }
+           }
+        }
+    });
+
+    workerThread.start();
+}
+
+void sendData() throws IOException
+{
+    //String msg = myTextbox.getText().toString();
+	String msg = "d";
+    msg += "\n";
+    mmOutputStream.write(msg.getBytes());
+    myLabel.setText("Data Sent");
+}
+
+void closeBT() throws IOException
+{
+    stopWorker = true;
+    mmOutputStream.close();
+    mmInputStream.close();
+    mmSocket.close();
+    myLabel.setText("Bluetooth Closed");
+}	
 }
